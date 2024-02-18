a2fd68d0a76d66f93b4483031212af09b46fced6
Eduardo Sanchez <es23jr@gmail.com>
2024-02-18 14:39:15 -0500

    Refactor home page sidebar

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -21,39 +21,47 @@ const socials = [
   },
 ]
 
+const Socials = () => (
+  <div className="flex flex-row gap-8 mt-4">
+    {socials.map((item) => {
+      const Icon = item.icon
+
+      return (
+        <Link href={item.href} key={item.href} target="_blank">
+          <Icon
+            size={24}
+            className="text-white opacity-75 hover:text-tertiary-1 transition-colors cursor-pointer"
+          />
+        </Link>
+      )
+    })}
+  </div>
+)
+
+const PersonalInfo = () => (
+  <div className="gap-4 flex flex-col flex-start">
+    <Typography variant="h1" className="font-bold">
+      Eduardo Sanchez
+    </Typography>
+    <Typography variant="h3" className="font-normal text-xl">
+      Founder & Full stack developer at Transakt.
+    </Typography>
+    <Typography
+      variant="p"
+      affects={'removePMargin'}
+      className="opacity-75 text-md max-w-xs"
+    >
+      Developer based in New Jersey, spending my time building web apps that
+      help people.
+    </Typography>
+  </div>
+)
+
 const Sidebar = () => {
   return (
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24  gap-4 flex flex-col">
-      <div className="gap-4 flex flex-col flex-start">
-        <Typography variant="h1" className="font-bold">
-          Eduardo Sanchez
-        </Typography>
-        <Typography variant="h3" className="font-normal text-xl">
-          Founder & Full stack developer at Transakt.
-        </Typography>
-        <Typography
-          variant="p"
-          affects={'removePMargin'}
-          className="opacity-75 text-md max-w-xs"
-        >
-          Developer based in New Jersey, spending my time building web apps that
-          help people.
-        </Typography>
-      </div>
-      <div className="flex flex-row gap-8 mt-4">
-        {socials.map((item) => {
-          const Icon = item.icon
-
-          return (
-            <Link href={item.href} key={item.href} target="_blank">
-              <Icon
-                size={24}
-                className="text-white opacity-75 hover:text-tertiary-1 transition-colors cursor-pointer"
-              />
-            </Link>
-          )
-        })}
-      </div>
+      <PersonalInfo />
+      <Socials />
     </header>
   )
 }
