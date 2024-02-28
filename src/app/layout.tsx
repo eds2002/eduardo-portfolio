08c0be015874c2b7cd6cef9910b90cdc40fef088
Eduardo Sanchez <es23jr@gmail.com>
2024-02-28 09:47:50 -0500

    Add @vercel/analytics package and integrate analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,7 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-secondary-1">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
